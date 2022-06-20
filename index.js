7805099984cf82c1766988092db3eef27b272b3c
jack444410 <jack4444lovemiku@Gmail.com>
2022-06-20 17:30:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ input2.addEventListener("click", function (e) {
     }
 })
 
-reset.addEventListener("click", function(e) {
-    playeranswer=[];
-    gameRound=10;
+reset.addEventListener("click", function (e) {
+    playeranswer = [];
+    gameRound = 10;
     final_answer.innerHTML = '<p></p>';
 })
 
@@ -115,11 +115,11 @@ function guessNumber() {
             }, 5000);
         }
     }
+}
 
-    function checkLose() {
-        if (gameRound == 0) {
-            code = 'lose'
-            final_answer.innerHTML = `<p class="guess_number">遊戲結束。你已用盡次數，正確的答案為 ${randomnumber1} <p/><p class="guess_number">你已猜過了：${playeranswer}<p/>`
-        }
+function checkLose() {
+    if (gameRound == 0) {
+        code = 'lose'
+        final_answer.innerHTML = `<p class="guess_number">遊戲結束。你已用盡次數，正確的答案為 ${randomnumber1} <p/><p class="guess_number">你已猜過了：${playeranswer}<p/>`
     }
 }
